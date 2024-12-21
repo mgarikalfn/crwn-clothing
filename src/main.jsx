37321bc60d104514487a557fd0a43229b117a295
Girum kenenisa <134688283+mgarikalfn@users.noreply.github.com>
2024-12-21 09:09:14 -0800

    adding cart context and shopping icon as well as cart dropdown

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context.jsx";
+import { ProductsProvider } from "./contexts/products.context.jsx";
+import { CartProvider } from "./contexts/cart.context.jsx";
 import "./index.scss";
 import App from "./App.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </StrictMode>
   </BrowserRouter>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CrwnLogo from "../../assets/crown.svg";
 import "../navigation/navigation.component.scss";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import {SignOutUser} from "../../utils/firebase.utils"
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  
+  const {isCartOpen} = useContext(CartContext);
+
   return (
     <Fragment>
       <div className="navigation">
@@ -29,7 +33,10 @@ const Navigation = () => {
               Sign-In
             </Link>
           )}
+
+          <CartIcon />
         </div>
+        {isCartOpen  && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
